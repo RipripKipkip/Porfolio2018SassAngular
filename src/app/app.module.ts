4a20342cc1e34ab0c3794b84dc93b49a87c612ac
Kipkip <kipkipdesigns@gmail.com>
2018-01-17 10:54:58 +0100

    Menu + Footer + Social Menu + sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { MainFooterComponent } from './main-footer/main-footer.component';
+import { SocialMenuComponent } from './social-menu/social-menu.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    MainFooterComponent
+    MainFooterComponent,
+    SocialMenuComponent
   ],
   imports: [
     BrowserModule
